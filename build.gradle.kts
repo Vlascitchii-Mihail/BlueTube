b8f8187ba654b34a35abd8a71ee7bef58944a3aa
Mihail <mishavlashitschii@gmail.com>
2024-02-27 18:23:47 +0200

    TSK-ID-2 Gradle Version Catalogue * add Gradle Version Catalogue

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,17 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
+buildscript {
+    repositories {
+        google()
+    }
+    dependencies {
+        classpath(libs.safe.args)
+    }
+}
 plugins {
-    id("com.android.application") version "8.2.2" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.22" apply false
+    alias(libs.plugins.android.application) apply false
+    alias(libs.plugins.kotlin.android) apply false
+    alias(libs.plugins.kapt) apply false
+    alias(libs.plugins.ksp) apply false
+    alias(libs.plugins.hilt) apply false
+    alias(libs.plugins.safe.args.plugin) apply false
 }
