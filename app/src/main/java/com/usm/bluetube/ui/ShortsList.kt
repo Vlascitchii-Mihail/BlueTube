386cfdcd9910e3602cdac272d5d915a45321f7c5
Marty McFly <99603411+Vlascitchii-Mihail@users.noreply.github.com>
2024-02-27 18:42:37 +0200

    TSK-ID-3 Video and Shorts Fragments (#2)
    * add Video and Shorts Fragments
    * add new app icon and delete old
    * add dimens.xml

---
+++ app/src/main/java/com/usm/bluetube/ui/ShortsList.kt
@@ -0,0 +1,14 @@
+package com.usm.bluetube.ui
+
+import android.os.Bundle
+import android.view.View
+import com.usm.bluetube.BaseFragment
+import com.usm.bluetube.databinding.FragmentShortsListBinding
+import dagger.hilt.android.AndroidEntryPoint
+
+class ShortsList : BaseFragment<FragmentShortsListBinding>(FragmentShortsListBinding::inflate) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
