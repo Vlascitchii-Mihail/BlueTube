87a13d899bf53765d73748d68374e4cc90b05ff5
Mihail <mishavlashitschii@gmail.com>
2024-02-28 11:41:08 +0200

    TSK-ID-6 Bottom Navigation View * add Bottom Navigation View * add bottom navigation menu for Bottom Navigation View * add menu icons

--- app/src/main/java/com/usm/bluetube/MainActivity.kt
+++ app/src/main/java/com/usm/bluetube/MainActivity.kt
@@ -1,13 +1,28 @@
 package com.usm.bluetube
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import dagger.hilt.android.AndroidEntryPoint
+
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setupBottomNavigation()
+    }
+
+    private fun setupBottomNavigation() {
+        val navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.containerFragment) as NavHostFragment
+
+        val navController = navHostFragment.navController
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottom_nav)
+        bottomNavigationView.setupWithNavController(navController)
     }
 }
