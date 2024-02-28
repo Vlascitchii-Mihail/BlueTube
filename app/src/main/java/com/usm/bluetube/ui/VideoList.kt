069044ef9468b0d8acf6260424a5ec5eaa5d1c97
Marty McFly <99603411+Vlascitchii-Mihail@users.noreply.github.com>
2024-02-28 19:13:03 +0200

    TSK-ID-7 Toolbar (#7)
    * add Toolbar
    * add Toolbar styles
    * add Toolbar menu
    * add fonts

--- app/src/main/java/com/usm/bluetube/ui/VideoList.kt
+++ app/src/main/java/com/usm/bluetube/ui/VideoList.kt
@@ -1,8 +1,16 @@
 package com.usm.bluetube.ui
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.MenuHost
+import androidx.core.view.MenuProvider
 import com.usm.bluetube.BaseFragment
+import com.usm.bluetube.R
 import com.usm.bluetube.databinding.FragmentVideoListBinding
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -11,5 +19,34 @@ class VideoList : BaseFragment<FragmentVideoListBinding>(FragmentVideoListBindin
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+       setupView()
+    }
+
+    private fun setupView() {
+        setupAppBar()
+        setupToolbarMenu()
+    }
+
+    private fun setupAppBar() = with(binding) {
+        val appCompatActivity  = activity as AppCompatActivity
+        appCompatActivity.setSupportActionBar(toolBar)
+    }
+
+    private fun setupToolbarMenu() {
+        (requireActivity() as MenuHost).addMenuProvider(object: MenuProvider {
+            override fun onCreateMenu(menu: Menu, menuInflater: MenuInflater) {
+                menuInflater.inflate(R.menu.toolbar_menu, menu)
+            }
+
+            override fun onMenuItemSelected(menuItem: MenuItem): Boolean {
+                when(menuItem.itemId) {
+                    R.id.ic_user -> Toast.makeText(requireContext(), "User icon", Toast.LENGTH_LONG).show()
+                    R.id.ic_notify_bell -> Toast.makeText(requireContext(), "Notify", Toast.LENGTH_LONG).show()
+                    R.id.ic_search_video -> Toast.makeText(requireContext(), "Search", Toast.LENGTH_LONG).show()
+                }
+                return false
+            }
+        }, viewLifecycleOwner)
     }
 }
